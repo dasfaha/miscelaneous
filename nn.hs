bde395e67fcdbb2b33c4086c7579bf4f2a1b1d7f
dasfaha <davidasfaha@gmail.com>
2012-12-30 11:07:10 +0000

    Added error handling

--- nn.hs
+++ nn.hs
@@ -1,8 +1,8 @@
---www.haskell.org/haskellwiki/99_questions/1_to_10
+--www.haskell.org/haskellwiki/99_questions
 
 --3
 elementAt           :: [a]-> Int -> a
 elementAt (x:xs) i  
     | i == 1 = x  
     | otherwise = elementAt xs (i-1)
-elementAt [] i       = error "Can't get element from empty list"
+elementAt [] i       = error "Can't get element from empty list/Index out of bounds"
